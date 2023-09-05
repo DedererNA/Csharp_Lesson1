9ae507c8b4aa59f4ce760739ae08420e9c5214fd
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-05 22:52:29 +0300

    task8 finished

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+while (b<a)
+{
+    b++;
+    if (b%2==0)
+    {
+        Console.Write(b);
+        if (b<a)
+        {
+            Console.Write(", ");
+        }
+    }
+}
