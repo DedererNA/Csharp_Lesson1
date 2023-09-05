6595a7056e45cfc9b53082edca13e30c8dec3e84
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-05 22:38:48 +0300

    task4 finished

---
+++ task4/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a>=b)
+{
+    if (a>=c)
+    {
+        Console.Write("Max=");
+        Console.Write(a);
+    }
+    else
+    {
+        Console.Write("Max=");
+        Console.Write(c);
+    }
+}
+else
+{
+    if (b>=c)
+    {
+        Console.Write("Max=");
+        Console.Write(b);
+    }
+    else
+    {
+        Console.Write("Max=");
+        Console.Write(c);
+    }
+}
